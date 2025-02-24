228e449d3426da680e406ab3b670169c762ad402
KKS-Constant0fps <konstantin.s.kozlov@gmail.com>
2025-02-24 20:54:39 +0500

    moved registration to a separate composer

--- src/mod.ts
+++ src/mod.ts
@@ -2,17 +2,17 @@ import { Bot, Context, InlineKeyboard, session, SessionFlavor } from "grammy";
 import { freeStorage } from "https://deno.land/x/grammy_storages@v2.4.2/free/src/mod.ts";
 import { entryComposer } from "./composers/entry.ts";
 import { channelComposer } from "./composers/channel.ts";
+import { registryComposer } from "./composers/registry.ts";
 import { channelKey, deletePost, getPost } from "./db/channel.ts";
-import { getProfile, setProfile } from "./db/profile.ts";
 import { monthAsText } from "./db/offload.ts";
 
-interface SessionData {
+export interface SessionData {
   registryStatus?: "name" | "surname" | "paid";
   name?: string;
   surname?: string;
 }
 
-type BotContext = Context & SessionFlavor<SessionData>;
+export type BotContext = Context & SessionFlavor<SessionData>;
 
 export const bot = new Bot<BotContext>(Deno.env.get("TOKEN") || "");
 export const kv = await Deno.openKv();
@@ -40,62 +40,6 @@ bot.chatType("private").command("data", async (ctx) => {
   }
 });
 
-// register
-bot.chatType("private").command("register", async (ctx) => {
-  const profile = await getProfile(ctx.from.id);
-  if (!profile) {
-    ctx.session.registryStatus = "name";
-    await ctx.reply("Напиши свое имя");
-  } else {
-    await ctx.reply("Ты уже зарегистрирован");
-  }
-});
-// get name
-bot.chatType("private")
-  .filter((ctx) => checkStatus(ctx, "name"))
-  .on("msg:text", async (ctx) => {
-    ctx.session.name = ctx.msg.text;
-    ctx.session.registryStatus = "surname";
-    await ctx.reply("Теперь напиши свою фамилию");
-  });
-// got surname
-bot.chatType("private")
-  .filter((ctx) => checkStatus(ctx, "surname"))
-  .on("msg:text", async (ctx) => {
-    ctx.session.surname = ctx.msg.text;
-    ctx.session.registryStatus = "paid";
-    const keyboard = new InlineKeyboard();
-    keyboard.text("Да ✅", "yes").text("Нет ❌", "no");
-    await ctx.reply("Ты бесплатник?", { reply_markup: keyboard });
-  });
-// if free cafeteria
-bot.chatType("private")
-  .filter((ctx) => checkStatus(ctx, "paid"))
-  .callbackQuery("yes", async (ctx) => {
-    ctx.session.registryStatus = undefined;
-    await setProfile(
-      ctx.from.id,
-      ctx.session.name || "",
-      ctx.session.surname || "",
-      true,
-    );
-    await ctx.editMessageText(ctx.msg?.text + `\n\nДа ✅`);
-    await ctx.reply("Сделано!");
-  });
-// if paid cafeteria
-bot.chatType("private")
-  .filter((ctx) => checkStatus(ctx, "paid"))
-  .callbackQuery("no", async (ctx) => {
-    ctx.session.registryStatus = undefined;
-    await setProfile(
-      ctx.from.id,
-      ctx.session.name || "",
-      ctx.session.surname || "",
-      false,
-    );
-    await ctx.editMessageText(ctx.msg?.text + `\n\nНет ❌`);
-    await ctx.reply("Сделано!");
-  });
 // add channel to approved list
 bot.chatType("private").command("add", async (ctx) => {
   const channelId = Number(ctx.match);
@@ -117,6 +61,7 @@ bot.callbackQuery(
     }),
 );
 
+bot.use(registryComposer);
 bot.use(entryComposer);
 bot.use(channelComposer);
 
@@ -135,6 +80,3 @@ kv.listenQueue(async (value: { channelId: number; date: Date }) => {
 });
 
 bot.catch(console.error);
-
-const checkStatus = (ctx: BotContext, status: SessionData["registryStatus"]) =>
-  ctx.session.registryStatus == status;
