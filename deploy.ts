ff66fbec10ad1988b8d155b35f7c264c244283fb
Constant10fps <konstantin.s.kozlov@gmail.com>
2025-12-08 22:13:10 +0500

    add acao header

--- deploy.ts
+++ deploy.ts
@@ -35,7 +35,10 @@ app.post("/auth", async (req, res) => {
   }
   try {
     const token = await auth(req.body.initData as string);
-    res.status(200).cookie("Authorization", token, { httpOnly: true });
+    res.status(200).setHeader(
+      "Access-Control-Allow-Origin",
+      "https://constant0fps-react-site.vercel.app",
+    ).cookie("Authorization", token, { httpOnly: true });
   } catch (error) {
     res.status(400).send(error);
   }
