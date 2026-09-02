3d2659e74e680ef9152804c8e7bcf5f69de41270
Alexandr Gonin <alexandrgonin000@gmail.com>
2026-09-02 17:05:34 +0500

    Update mod.ts

--- src/mod.ts
+++ src/mod.ts
@@ -68,7 +68,7 @@ bot.use(entryComposer);
 bot.use(channelComposer);
 
 // post opening
-Deno.cron("daily entry", "5 12 * * 1-6", async () => {
+Deno.cron("daily entry", "7 12 * * 1-6", async () => {
   const open = (await kv.get<boolean>(["open"])).value;
   if (!open) return;
   const delay = 3 * 60 * 60 * 1000;
